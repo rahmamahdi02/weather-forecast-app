fb7dbd18ce440def89a133699804d1195c348442
TPL1122_18 <tpl1122_18@TPL1122-18s-MacBook-Pro.local>
2023-03-02 21:41:00 -0800

    fix form

--- client/src/components/weatherForm.js
+++ client/src/components/weatherForm.js
@@ -4,14 +4,13 @@ const WeatherForm = (props) =>{
     return (
         <div className="weather">
         <h1 className="App-header">Techtonica Weather Forecast App</h1>
-        <form onSubmit={props.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <input
             id="city-name"
             type="text"
             placeholder="Please enter the city name"
             name="city"
-            value={props.city}
-            readOnly
+            onChange={onChange}
           />
           <input type="submit" value="Submit" />
         </form>
